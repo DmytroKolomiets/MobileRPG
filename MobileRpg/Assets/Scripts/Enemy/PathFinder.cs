a4c648a61b7ae00272fe1cb154219a897ef4440d
DmytroKolomiets <58811592+DmytroKolomiets@users.noreply.github.com>
2020-12-22 23:58:26 +0200

    Added PathFinding correction for radial system

--- MobileRpg/Assets/Scripts/Enemy/PathFinder.cs
+++ MobileRpg/Assets/Scripts/Enemy/PathFinder.cs
@@ -13,7 +13,7 @@ public class PathFinder
             return currentPosition;
         }
             
-        return GetClosestPoint(desireblePosition.Index, freeSnapPointsInRange);
+        return GetClosestPoint(desireblePosition.Index, freeSnapPointsInRange, snapPoints.GetLength(0));
     }
     private void GetFreeSnapPointsInRange(Vector2Int positon, SnapPoint[,] snapPoints, int range)
     {
@@ -21,28 +21,34 @@ public class PathFinder
         freeSnapPointsInRange.Clear();
         foreach (var item in snapPoints)
         {         
-            if (GetDistance(positon, item.Index) < range && item.IsFree)
+            if (GetDistance(positon, item.Index, snapPoints.GetLength(0)) < range && item.IsFree)
             {
                 freeSnapPointsInRange.Add(item);
             }
         }
     }
-    private SnapPoint GetClosestPoint(Vector2Int desireblePosition, List<SnapPoint> freeSnapPoints)
+    private SnapPoint GetClosestPoint(Vector2Int desireblePosition, 
+        List<SnapPoint> freeSnapPoints, int arrayXLenth)
     {
         SnapPoint snapPoint = freeSnapPoints[0];
-        int distance = GetDistance(snapPoint.Index, desireblePosition);
+        int distance = GetDistance(snapPoint.Index, desireblePosition, arrayXLenth);
         foreach (var item in freeSnapPoints)
         {
-            if (GetDistance(desireblePosition, item.Index) < distance)
+            if (GetDistance(desireblePosition, item.Index, arrayXLenth) < distance)
             {
                 snapPoint = item;
-                distance = GetDistance(item.Index, desireblePosition);
+                distance = GetDistance(item.Index, desireblePosition, arrayXLenth);
             }
         }
         return snapPoint;
     }
-    private int GetDistance(Vector2Int a, Vector2Int b)
+    private int GetDistance(Vector2Int a, Vector2Int b, int arrayXLenth)
     {
-        return (Mathf.Abs(a.x - b.x ) + Mathf.Abs(a.y - b.y)) / 2;
+        return (GetDistanceCorrectionForRadial(a.x, b.x, arrayXLenth) + Mathf.Abs(a.y - b.y)) / 2;
+    }
+    private int GetDistanceCorrectionForRadial(int aX, int bX,  int arrayXLenth)
+    {
+        int distance = Mathf.Abs(aX - bX);
+        return distance > (arrayXLenth / 2) ? arrayXLenth - distance : distance;
     }
 }
